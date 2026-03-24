5e463e690a51899250f457539e00a84ebc795e7b
wotiwan <iwanpomogaev@yandex.ru>
2026-03-24 22:32:45 +0800

    initial commit

---
+++ src/main/java/com/wotiwan/medonline/MedOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.wotiwan.medonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wotiwan/medonline/MedOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wotiwan.medonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
